bdff78c7d10aaee78bcd64cc2ff0ce9ab06be1e1
Tong Liu <tongliu.sh@gmail.com>
2020-08-26 01:14:16 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Tong Liu
+ * Date: 2020/08/19
+ *
+ * A simple hello world program in C
+ **/
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+	 printf("Hello World!\n");
+
+	 return 0;
+
+ }
